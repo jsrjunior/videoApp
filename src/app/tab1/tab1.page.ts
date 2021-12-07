ae7a88ea9b3d39e96178ecab4cb5a105cbf1bf05
Jose Soares Ramos Junior <jsr.junior.dev@gmail.com>
2021-12-07 17:11:09 -0300

    adicionado alert e toast

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,41 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public toastController: ToastController, public alertController: AlertController) {}
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Favoritado com sucesso!.',
+      duration: 2000,
+      color: 'success',
+    });
+    toast.present();
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar!',
+      message: 'Favoritar o filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'Confirmar',
+          handler: () => {
+            this.presentToast();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
 
 }
