e4e50920d766574f99a7010f2a2a148b27a95560
Jose Soares Ramos Junior <jsr.junior.dev@gmail.com>
2021-12-08 16:27:23 -0300

    utilizando lacos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,28 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos';
+
+  listaVideos: IFilme[] = [
+    {
+      cartaz: 'https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_UX67_CR0,0,67,98_AL_.jpg',
+      classificacao: 99,
+      duracao: 'Em Breve',
+      lancamento: 'Em Breve',
+      generos: ['Ação', 'Aventura'],
+      nome: 'Homem-Aranha: Sem Volta Para Casa'
+    },
+
+    {
+      cartaz: 'https://www.themoviedb.org/t/p/w220_and_h330_face/h5UzYZquMwO9FVn15R2eK2itmHu.jpg',
+      classificacao: 80,
+      duracao: '1H 30Min',
+      lancamento: '30 de set de 2021',
+      generos: ['Ação', 'Aventura'],
+      nome: 'Venom: Tempo de Carnificina'
+    }
+  ];
 
   constructor(public toastController: ToastController, public alertController: AlertController) {}
 
